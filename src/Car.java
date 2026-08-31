16a64163f6587e1d3d82e2523b8b16602ef7e7f9
khalid <cs.devlop@gmail.com>
2026-08-31 11:13:00 +0300

    add in this class Car get and set

--- src/Car.java
+++ src/Car.java
@@ -14,4 +14,28 @@ public class Car {
         this.fixedRoute = fixedRoute;
         this.maxCapacity =maxCapacity;
     }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public Route getFixedRoute() {
+        return fixedRoute;
+    }
+
+    public void setFixedRoute(Route fixedRoute) {
+        this.fixedRoute = fixedRoute;
+    }
+
+    public int getMaxCapacity() {
+        return maxCapacity;
+    }
+
+    public void setMaxCapacity(int maxCapacity) {
+        this.maxCapacity = maxCapacity;
+    }
 }
