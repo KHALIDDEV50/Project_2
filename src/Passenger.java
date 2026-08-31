69671bf722f7d2884a284179ecc6f7df05ff67f3
khalid <cs.devlop@gmail.com>
2026-08-31 11:20:19 +0300

    in this class create Varible and Constractor and add tow Methods (abstract reserveCar Method , displayDetails Method)

--- src/Passenger.java
+++ src/Passenger.java
@@ -1,2 +1,33 @@
-public class Passenger {
+abstract class Passenger  {
+
+    private String name;
+    private String id;
+    private Car reservedCar;
+    private double tripCost;
+
+    // Constructor..
+
+    public Passenger() {
+
+    }
+
+    public Passenger(String name, String id) {
+        this.name = name;
+        this.id = id;
+    }
+
+
+    // Methods..
+    public abstract void reserveCar(Car car) throws Exception;
+
+    public void displayDetails(){
+        System.out.println("Passenger Name: "+ name);
+        System.out.println("Passenger ID: "+id);
+        if (reservedCar != null){
+            System.out.println("Car Code: " + reservedCar.getCode());
+            System.out.println("Route Original Price: $" + reservedCar.getFixedRoute().getPrice());
+            System.out.println("Final Trip Cost: $" + tripCost);
+        }
+    }
+
 }
