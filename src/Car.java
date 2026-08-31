965ea8c42d757b5b66ee959c79bc921990d74777
khalid <cs.devlop@gmail.com>
2026-08-31 10:48:15 +0300

    init varlible in class Car and init Constructors

--- src/Car.java
+++ src/Car.java
@@ -1,2 +1,17 @@
 public class Car {
+    private String code;
+    private Route fixedRoute;
+    private int maxCapacity;
+
+    // Constructor
+
+    public Car(){
+
+    }
+
+    public  Car(String code, Route fixedRoute , int maxCapacity){
+        this.code = code;
+        this.fixedRoute = fixedRoute;
+        this.maxCapacity =maxCapacity;
+    }
 }
